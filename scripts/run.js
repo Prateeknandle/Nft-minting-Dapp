1642999d6e12b877ded5f76b07b7d06058399bc2
Prateeknandle <prateeknandle@gmail.com>
2022-03-02 18:30:16 +0530

    can mint one specific nft

---
+++ scripts/run.js
@@ -0,0 +1,23 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+
+    let txn = await nftContract.makeAnEpicNFT(); //calling the function
+    await txn.wait()//waiting for it to be mined
+    txn = await nftContract.makeAnEpicNFT()//mining another nft.
+    await txn.wait()
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
